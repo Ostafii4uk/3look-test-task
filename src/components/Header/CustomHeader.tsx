9915eb1b9d37d3dfb232fcb3d09afff02ca3f9c2
Ostafii4uk <ostafii4uk@gmail.com>
2023-11-06 21:28:49 +0200

    Added border for Header

--- src/components/Header/CustomHeader.tsx
+++ src/components/Header/CustomHeader.tsx
@@ -5,12 +5,14 @@ import logoIcon from '../../../public/assets/svgs/logo.svg'
 const CustomHeader: React.FC<{}> = ({}) => {
   return (
     <header className={Syles.header}>
-      <div className={Syles.logo}>
-        <Image src={logoIcon} alt='3look' priority={true} />
-        <h2 className={Syles.title}>Memes</h2>
-      </div>
-      <div className={Syles.search}>
-        <input type="text" className={Syles.inputSearch} placeholder='Search' />
+      <div className={Syles.headerContainer}>
+        <div className={Syles.logo}>
+          <Image src={logoIcon} alt='3look' priority={true} />
+          <h2 className={Syles.title}>Memes</h2>
+        </div>
+        <div className={Syles.search}>
+          <input type="text" className={Syles.inputSearch} placeholder='Search' />
+        </div>
       </div>
     </header>
   )
