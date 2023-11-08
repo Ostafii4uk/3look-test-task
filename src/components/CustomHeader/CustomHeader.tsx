9edc5e6f07df858678d1130b7b2f57767ed4571c
Ostafii4uk <ostafii4uk@gmail.com>
2023-11-08 17:33:08 +0200

    Fixed search input in mobile

--- src/components/CustomHeader/CustomHeader.tsx
+++ src/components/CustomHeader/CustomHeader.tsx
@@ -1,17 +1,20 @@
-import Syles from './CustomHeader.module.css'
+import Styles from './CustomHeader.module.css'
 import Image from 'next/image'
 import logoIcon from '../../../public/assets/svgs/logo.svg'
+import { useState } from 'react'
 
 const CustomHeader: React.FC<{searchCategories: Function}> = ({searchCategories}) => {
+  const [isActiveSearch, setIsActiveSearch] = useState<boolean>(false)
+
   return (
-    <div className={Syles.header}>
-      <div className={Syles.headerContainer}>
-        <div className={Syles.logo}>
+    <div className={Styles.header}>
+      <div className={Styles.headerContainer}>
+        <div className={Styles.logo}>
           <Image src={logoIcon} alt='3look' priority={true} />
-          <h2 className={Syles.title}>Memes</h2>
+          <h2 className={Styles.title}>Memes</h2>
         </div>
-        <div className={Syles.search}>
-          <input type="text" className={Syles.inputSearch} onChange={(event) => searchCategories(event.target.value)} placeholder='Search' />
+        <div className={`${Styles.search} ${isActiveSearch && Styles.searchMobile}`} onClick={() => setIsActiveSearch(true)}>
+          <input type="text" className={Styles.inputSearch} onBlur={() => setIsActiveSearch(false)} onChange={(event) => searchCategories(event.target.value)} placeholder='Search' />
         </div>
       </div>
     </div>
